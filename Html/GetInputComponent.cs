8083185f7d0c6feb993939c65a9ac9d6f7503727
Ayo Dahunsi <ayo@dolasoft.com>
2021-02-09 20:05:57 -0700

    Add project files.

---
+++ Html/GetInputComponent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using ZeraSystems.CodeNanite.Expansion;
+using ZeraSystems.CodeStencil.Contracts;
+
+namespace ZeraSystems.CodeNanite.Cshtml
+{
+    /// <summary>
+    /// There are 6 elements in the String Array used by the
+    /// 0 - Publisher : This is the name of the publisher
+    /// 1 - Title : This is the title of the Code Nanite
+    /// 2 - Details : This is the
+    /// 3 - Version Number
+    /// 4 - Label : Label of the Code Nanite
+    /// 5 - Namespace
+    /// 6 - Release Date
+    /// 7 - Name to use for Expander Label
+    /// 8 - Indicates that the Nanite is Schema Dependent
+    /// 9 - RESERVED
+    /// 10 - RESERVED
+    /// </summary>
+    [Export(typeof(ICodeStencilCodeNanite))]
+    [CodeStencilCodeNanite(new[]
+    {
+        // 0
+        "Zera Systems Inc.",  
+        // 1
+        "Returns an Input Component",    
+        // 2
+        "Returns an Input Component based on the data type of the column..",                                   
+        // 3
+        "1.0",                                  
+        // 4
+        "GetInputComponent",                         
+        // 5
+        "ZeraSystems.CodeNanite.Cshtml",               
+        // 6
+        "07/02/2020",                           
+        // 7
+        "CS_CSHTML_INPUT",                        
+        // 8
+        "1",                                   
+        // 9
+        "", 
+        // 10
+        ""                                      
+    })]
+    public partial class GetInputComponent : ExpansionBase, ICodeStencilCodeNanite
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+        public int Counter { get; set; }
+        public List<string> OutputList { get; set; }
+        public List<ISchemaItem> SchemaItem { get; set; }
+        public List<IExpander> Expander { get; set; }
+        public List<string> InputList { get ; set; }
+
+        public void ExecutePlugin()
+        {
+            Initializer(SchemaItem, Expander);
+            MainFunction();
+        }
+    }
+}
---
+++ Htmlz/Lookup/GenerateLookups.Functions.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ZeraSystems.CodeNanite.Expansion;
+
+//using ZeraSystems.CodeNanite.Expansion;
+using ZeraSystems.CodeStencil.Contracts;
+
+namespace ZeraSystems.CodeNanite.Cshtml
+{
+    public partial class GenerateLookups
+    {
+        private string _public = "public ";
+        private string _getSet = " { get; set; }";
+        private string _table;
+        private List<ISchemaItem> _foreignKeys;
+        private List<ISchemaItem> _selfJoinColumns;
+
+        private void MainFunction()
+        {
+            _table = GetTable( Singularize(Input,PreserveTableName()), false);
+            _foreignKeys = GetForeignKeysInTable(_table);
+                //.Where(t=>t.OriginalName==t.LookupColumn).ToList();
+            _selfJoinColumns = GetSelfJoinColumns(_table);
+
+            //_foreignKeys = GetNavProperties(_table);
+
+            AppendText();
+            //if (!_foreignKeys.Any())
+            //{
+            //    AppendText(Indent(8)+"// Add your code here if you are creating lookup manually.");
+            //    return;
+            //}
+            AppendText(GetEachLookup(), string.Empty); // This is not to allow line feed
+        }
+
+        private string GetEachLookup()
+        {
+            BuildSnippet(null);
+            if (_foreignKeys.Any())
+                foreach (var item in _foreignKeys)
+                    CreateLookup(item);
+            else if (_selfJoinColumns.Any())
+                foreach (var item in _selfJoinColumns)
+                    CreateLookup(item);
+            else
+            {
+                AppendText(Indent(8) + "// Add your code here if you are creating lookup manually.");
+            }
+
+            return BuildSnippet();
+
+
+            void CreateLookup(ISchemaItem item)
+            {
+                var selectList = item.RelatedTable + "SelectList";
+                var key = GetPrimaryKey(item.RelatedTable);
+                var lookupDisplay = GetLookupDisplayColumn(_table, item.ColumnName);
+                //var selectList = "Select" + item.RelatedTable;
+                var query = Pluralize(item.RelatedTable,PreserveTableName()).ToLower() + "Query";
+                var selectedTable = "selected" + item.RelatedTable;
+                BuildSnippet(_public + "SelectList " + selectList + _getSet, 8);
+                BuildSnippet("");
+                BuildSnippet(_public + " void " + "Populate" + CreateTablePropertyName(item) + "Lookup(" + GetDbContext() +
+                             " context, object " + selectedTable + " = null)", 8);
+                BuildSnippet("{", 8);
+
+                BuildSnippet(
+                    "var " + query + " = from q in context." + Singularize(item.RelatedTable,PreserveTableName()) + " orderby q." + lookupDisplay +
+                    " select q;", 12);
+                //BuildSnippet("");
+                BuildSnippet(selectList + " = new SelectList(" + query + ".AsNoTracking(), " +
+                             key.AddQuotes() + ", " + lookupDisplay.AddQuotes() + ", " +
+                             selectedTable + ");", 12);
+                BuildSnippet("}", 8);
+                BuildSnippet("");
+            }
+        }
+
+
+
+    }
+}
